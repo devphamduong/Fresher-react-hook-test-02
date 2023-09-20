66b9f50f2fb8375567644894ee4ac798159a8c92
DuongPC <pduong244@gmail.com>
2023-09-20 23:02:43 +0700

    Tích hợp React Router

---
+++ src/components/pages/login/index.jsx
@@ -0,0 +1,9 @@
+function LoginPage() {
+    return (
+        <>
+            login
+        </>
+    );
+}
+
+export default LoginPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
@@ -10,4 +10,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </Provider>
   </React.StrictMode>
-)
+);
