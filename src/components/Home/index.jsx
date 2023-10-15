6d348a01b18cfa6f5169364fab070f16bde1d843
DuongPC <pduong244@gmail.com>
2023-10-15 14:52:04 +0700

    Filter/Sorter

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,5 @@
 import { FilterOutlined, ReloadOutlined } from "@ant-design/icons";
-import { Button, Card, Checkbox, Col, Divider, Form, InputNumber, Pagination, Rate, Row, Space, Tabs } from "antd";
+import { Button, Card, Checkbox, Col, Divider, Form, InputNumber, Pagination, Rate, Row, Space, Spin, Tabs } from "antd";
 import './Home.scss';
 import { getAllBookCategories, getBookPaginate } from "../../services/api";
 import { useEffect, useState } from "react";
@@ -10,6 +10,10 @@ function Home() {
     const [listBooks, setListBooks] = useState([]);
     const [current, setCurrent] = useState(1);
     const [total, setTotal] = useState(0);
+    const [pageSize, setPageSize] = useState(4);
+    const [filter, setFilter] = useState('');
+    const [sortQuery, setSortQuery] = useState('&sort=-sold');
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         fetchAllCategories();
@@ -17,7 +21,7 @@ function Home() {
 
     useEffect(() => {
         fetchBook();
-    }, [current]);
+    }, [current, pageSize, filter, sortQuery]);
 
     const fetchAllCategories = async () => {
         let res = await getAllBookCategories();
@@ -27,30 +31,60 @@ function Home() {
     };
 
     const fetchBook = async () => {
-        let query = `current=${current}&pageSize=4`;
+        setLoading(true);
+        let query = `current=${current}&pageSize=${pageSize}`;
+        if (filter) {
+            query += filter;
+        }
+        if (sortQuery) {
+            query += sortQuery;
+        }
         let res = await getBookPaginate(query);
         if (res && res.data) {
             setListBooks(res.data.result);
             setTotal(res.data.meta.total);
         }
+        setLoading(false);
     };
 
     const onChangeTab = (key) => {
-        console.log(key);
+        setSortQuery(key);
     };
 
     const handleChangeFilter = (changeValues, values) => {
-        console.log(values);
+        if (changeValues.categories) {
+            const categories = values.categories;
+            if (categories && categories.length > 0) {
+                const filters = categories.join();
+                setFilter(`&category=${filters}`);
+            } else {
+                setFilter('');
+            }
+        }
     };
 
     const onFinish = (values) => {
-        console.log(values);
+        let filters = '';
+        if (values?.range?.min >= 0) {
+            filters += `&price>=${values?.range?.min}`;
+            if (values?.range?.max >= 0) {
+                filters += `&price<=${values?.range?.max}`;
+            }
+            if (values?.categories?.length) {
+                const categories = values.categories.join();
+                filters += `&category=${categories}`;
+            }
+            setFilter(filters);
+        }
     };
 
-    const onChange = (page) => {
+    const onChange = (page, pagesize) => {
         if (page && page !== current) {
-            console.log(page);
             setCurrent(page);
+        }
+        if (pagesize !== pageSize) {
+            setPageSize(pagesize);
+            setCurrent(1);
         }
     };
 
@@ -63,7 +97,7 @@ function Home() {
                             <Col>
                                 <FilterOutlined style={{ color: '#4c96ff' }} /><span style={{ fontSize: 15, fontWeight: 'bold' }}> Filter</span>
                             </Col>
-                            <Col><Button type='ghost' icon={<ReloadOutlined />}></Button></Col>
+                            <Col><Button type='ghost' icon={<ReloadOutlined />} onClick={() => { form.resetFields(); setFilter(''); }}></Button></Col>
                         </Row>
                         <Form
                             form={form}
@@ -120,56 +154,58 @@ function Home() {
                         <Tabs defaultActiveKey="1" style={{ width: '100%' }}
                             items={[
                                 {
-                                    key: '1',
+                                    key: '&sort=-sold',
                                     label: 'Popular',
                                     children: <></>,
                                 },
                                 {
-                                    key: '2',
+                                    key: '&sort=updatedAt',
                                     label: 'New',
                                     children: <></>,
                                 },
                                 {
-                                    key: '3',
+                                    key: '&sort=price',
                                     label: 'Price low to high',
                                     children: <></>,
                                 },
                                 {
-                                    key: '4',
+                                    key: '&sort=-price',
                                     label: 'Price high to low',
                                     children: <></>,
                                 },
                             ]}
                             onChange={onChangeTab} />
                     </Row>
-                    <Row style={{ flexWrap: 'wrap' }} justify={'space-evenly'}>
-                        {listBooks && listBooks.length > 0 &&
-                            listBooks.map((item, index) => {
-                                return (
-                                    <Col key={`book-${index}`}>
-                                        <Card
-                                            hoverable
-                                            style={{ width: 240 }}
-                                            cover={<img alt="example" src={`${import.meta.env.VITE_BACKEND_URL}/images/book/${item.thumbnail}`} />}
-                                        >
-                                            <Card.Meta
-                                                title={item.mainText}
-                                                className={`title ${item.mainText.length > 40 ? 'ellipsis' : ''}`}
-                                                description={
-                                                    <>
-                                                        <div className="price" style={{ color: 'black' }}>{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(item.price)}</div>
-                                                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                                                            <Rate className="rate" defaultValue={5} disabled={true} style={{ fontSize: 15 }} />
-                                                            <span className="sold" style={{ marginLeft: 5, color: 'black' }}>{item.sold} sold</span></div>
-                                                    </>
-                                                } />
-                                        </Card>
-                                    </Col>
-                                );
-                            })}
-                    </Row>
-                    <Divider />
-                    <Pagination style={{ textAlign: 'center' }} current={current} total={total} pageSize={4} showSizeChanger={false} onChange={onChange} />
+                    <Spin spinning={loading} tip="Loading...">
+                        <Row style={{ flexWrap: 'wrap' }} justify={'space-evenly'}>
+                            {listBooks && listBooks.length > 0 &&
+                                listBooks.map((item, index) => {
+                                    return (
+                                        <Col key={`book-${index}`}>
+                                            <Card
+                                                hoverable
+                                                style={{ width: 240 }}
+                                                cover={<img alt="example" src={`${import.meta.env.VITE_BACKEND_URL}/images/book/${item.thumbnail}`} />}
+                                            >
+                                                <Card.Meta
+                                                    title={item.mainText}
+                                                    className={`title ${item.mainText.length > 40 ? 'ellipsis' : ''}`}
+                                                    description={
+                                                        <>
+                                                            <div className="price" style={{ color: 'black' }}>{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(item.price)}</div>
+                                                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                                                <Rate className="rate" defaultValue={5} disabled={true} style={{ fontSize: 15 }} />
+                                                                <span className="sold" style={{ marginLeft: 5, color: 'black' }}>{item.sold} sold</span></div>
+                                                        </>
+                                                    } />
+                                            </Card>
+                                        </Col>
+                                    );
+                                })}
+                        </Row>
+                        <Divider />
+                        <Pagination responsive style={{ textAlign: 'center' }} current={current} total={total} pageSize={pageSize} showSizeChanger={false} onChange={onChange} />
+                    </Spin>
                 </Col>
             </Row>
         </div>
