f1b53a8bb496137e0388518c35e722323a5ebaae
DuongPC <pduong244@gmail.com>
2023-11-12 22:39:56 +0700

    Payment Page

--- src/pages/order/index.jsx
+++ src/pages/order/index.jsx
@@ -1,39 +1,38 @@
-import { Button, Col, Divider, InputNumber, Row, Typography } from "antd";
+import { Button, Col, Divider, Form, Input, InputNumber, Radio, Result, Row, Steps, Typography, message } from "antd";
 import { useDispatch, useSelector } from "react-redux";
-import { DeleteOutlined } from '@ant-design/icons';
+import { DeleteOutlined, SmileOutlined } from '@ant-design/icons';
 import { addToCartAction, removeProductAction } from '../../redux/order/orderSlice';
 import { useEffect, useState } from "react";
+import './Order.scss';
 
 function OrderPage(props) {
     const carts = useSelector(state => state.orders.carts);
+    const user = useSelector(state => state.account.user);
     const dispatch = useDispatch();
     const [totalPrice, setTotalPrice] = useState(carts.reduce((total, item) => total + (item.quantity * item.detail.price), 0));
+    const [current, setCurrent] = useState(0);
+    const [form] = Form.useForm();
 
-    const onChange = (value, book) => {
-        if (value && !value < 1) {
-            dispatch(addToCartAction({ quantity: value, _id: book._id, detail: book, action: 'cart' }));
-        }
-    };
-
-    const handleRemoveProduct = (id) => {
-        if (id) {
-            dispatch(removeProductAction(id));
-        }
+    const onFinish = (values) => {
+        next();
     };
 
     useEffect(() => {
-        setTotalPrice(carts.reduce((total, item) => total + (item.quantity * item.detail.price), 0));
-    }, [carts]);
+        if (current === steps.length - 1) {
+            message.success('Order successfully!');
+        }
+    }, [current]);
 
-    return (
-        <div style={{ padding: '0 40px' }}>
-            {carts.length > 0 ?
-                <Row justify={'space-evenly'}>
+    const steps = [
+        {
+            title: 'Cart',
+            content:
+                <>
                     <Col span={17}>
                         {
                             carts.map((item, index) => {
                                 return (
-                                    <Row align={'middle'} justify={'space-between'} style={index === 0 ? { backgroundColor: 'white', margin: 0, padding: '10px 20px', borderRadius: 5 } : { backgroundColor: 'white', margin: '15px 0', padding: '10px 20px', borderRadius: 5 }}>
+                                    <Row align={'middle'} gutter={[10, 0]} style={index === 0 ? { backgroundColor: 'white', margin: 0, padding: '10px 20px', borderRadius: 5 } : { backgroundColor: 'white', margin: '15px 0', padding: '10px 20px', borderRadius: 5 }}>
                                         <Col span={2}>
                                             <img src={`${import.meta.env.VITE_BACKEND_URL}/images/book/${item.detail.thumbnail}`} alt="" width={80} height={80} />
                                         </Col>
@@ -43,7 +42,7 @@ function OrderPage(props) {
                                         <Col span={3}>
                                             {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(item.detail.price ?? 0)}
                                         </Col>
-                                        <Col span={2}>
+                                        <Col span={3}>
                                             <InputNumber min={1} max={item.detail.quantity} defaultValue={item.quantity} onChange={(value) => onChange(value, item.detail)} />
                                         </Col>
                                         <Col span={5} style={{ textAlign: 'center' }}>
@@ -57,7 +56,7 @@ function OrderPage(props) {
                             })
                         }
                     </Col>
-                    <Col span={6}>
+                    <Col span={7}>
                         <div style={{ backgroundColor: 'white', padding: '20px 20px', borderRadius: 5 }}>
                             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                                 <span>Temporarily calculated</span><span style={{ color: 'red' }}>{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(totalPrice)}</span>
@@ -67,13 +66,130 @@ function OrderPage(props) {
                                 <span>Total</span><span style={{ color: 'red' }}>{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(totalPrice)}</span>
                             </div>
                             <Divider></Divider>
-                            <div style={{}}><Button block className='button button-buy' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>Purchase ({carts.length})</Button></div>
+                            <div><Button block className='button button-buy' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} onClick={() => next()}>Purchase ({carts.length})</Button></div>
                         </div>
                     </Col>
-                </Row>
-                :
-                <div style={{ textAlign: 'center' }}>No Product</div>}
-        </div >
+                </>,
+        },
+        {
+            title: 'Order',
+            content:
+                <>
+                    <Col span={17}>
+                        {
+                            carts.map((item, index) => {
+                                return (
+                                    <Row align={'middle'} gutter={[10, 0]} style={index === 0 ? { backgroundColor: 'white', margin: 0, padding: '10px 20px', borderRadius: 5 } : { backgroundColor: 'white', margin: '15px 0', padding: '10px 20px', borderRadius: 5 }}>
+                                        <Col span={2}>
+                                            <img src={`${import.meta.env.VITE_BACKEND_URL}/images/book/${item.detail.thumbnail}`} alt="" width={80} height={80} />
+                                        </Col>
+                                        <Col span={10}>
+                                            <Typography.Text ellipsis>{item.detail.mainText}</Typography.Text>
+                                        </Col>
+                                        <Col span={3}>
+                                            {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(item.detail.price ?? 0)}
+                                        </Col>
+                                        <Col span={3}>
+                                            Quantity: {item.detail.quantity}
+                                        </Col>
+                                        <Col span={6} style={{ textAlign: 'center' }}>
+                                            Total: {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(item.quantity * item.detail.price ?? 0)}
+                                        </Col>
+                                    </Row>
+                                );
+                            })
+                        }
+                    </Col>
+                    <Col span={7}>
+                        <div style={{ backgroundColor: 'white', padding: '20px 20px', borderRadius: 5 }}>
+                            <Form
+                                form={form}
+                                onFinish={onFinish}
+                                layout="vertical"
+                            >
+                                <Form.Item name="receiver" label="Receiver" rules={[{ required: true }]} initialValue={user.fullName}>
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item name="phone" label="Phone number" rules={[{ required: true }]} initialValue={user.phone}>
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item name="address" label="Address" rules={[{ required: true }]}>
+                                    <Input.TextArea rows={4} />
+                                </Form.Item>
+                                <Form.Item name="payment" label="Payment type">
+                                    <Radio.Group value={1}>
+                                        <Radio value="1">Payment on delivery</Radio>
+                                    </Radio.Group>
+                                </Form.Item>
+                            </Form>
+                            <Divider ></Divider>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <span>Total</span><span style={{ color: 'red' }}>{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(totalPrice)}</span>
+                            </div>
+                            <Divider></Divider>
+                            <div style={{ display: 'flex', gap: 10 }} >
+                                <Button className='button button-buy' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '50%' }} onClick={() => prev()}>Previous</Button>
+                                <Button className='button button-buy' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '50%' }} onClick={() => form.submit()}>Place order ({carts.length})</Button>
+                            </div>
+                        </div>
+                    </Col>
+                </>,
+        },
+        {
+            title: 'Purchase',
+            content:
+                <Col span={24}>
+                    <Result
+                        icon={<SmileOutlined />}
+                        title="The order has been placed successfully!"
+                        extra={
+                            <Button type="primary" key="console">
+                                View order history
+                            </Button>
+                        }
+                    />
+                </Col>,
+        },
+    ];
+
+    const onChange = (value, book) => {
+        if (value && !value < 1) {
+            dispatch(addToCartAction({ quantity: value, _id: book._id, detail: book, action: 'cart' }));
+        }
+    };
+
+    const next = () => {
+        setCurrent(current + 1);
+    };
+
+    const prev = () => {
+        setCurrent(current - 1);
+    };
+
+    const items = steps.map((item) => ({ key: item.title, title: item.title }));
+
+    const handleRemoveProduct = (id) => {
+        if (id) {
+            dispatch(removeProductAction(id));
+        }
+    };
+
+    useEffect(() => {
+        setTotalPrice(carts.reduce((total, item) => total + (item.quantity * item.detail.price), 0));
+    }, [carts]);
+
+    return (
+        <>
+            <div style={{ padding: '0 40px' }} className="order-container">
+                {carts.length > 0 ?
+                    <Row justify={'space-between'} gutter={[16, 10]}>
+                        <Col span={24}><Steps style={{ padding: '10px 20px', backgroundColor: 'white', borderRadius: 5 }} current={current} items={items} status={current === steps.length - 1 ? "finish" : "finish"} /></Col>
+                        {steps[current].content}
+                    </Row>
+                    :
+                    <div style={{ textAlign: 'center' }}>No Product</div>}
+            </div >
+        </>
     );
 }
 
